1d0afe023fee0d338eccf88cb7f03be77b39f5ca
Revan <quentin.chaufour@etu.univ-orleans.fr>
2025-09-18 12:27:50 +0200

    trigger to verify budget constraints on campagne

--- RENDU_P1.md
+++ RENDU_P1.md
@@ -13,8 +13,8 @@ Nous avons utilisé le site [Mocodo](https://www.mocodo.net/) pour produire ce m
 
 #### Autres dépendances 
 
-- Les chercheurs participant a une campagne doivent les habilitations nécessaires a l'utilisation de la plateforme choisi.
+- Dans une campagne, toutes les habilitations nécessaires doivent être représentés au sein des chercheurs participants a la campagne.
 -  Le coût d'une campagne ne doit pas excéder le budget restant du mois du démarage de celle-ci.
-- Une plateforme n'est pas en cours / ne nécessite pas de maintenance lors du démarage d'une campagne.
+- Une plateforme n'est pas en cours de maintenance lors du démarage d'une campagne.
 - La plateforme utilisé durant une campagne ne doit pas avoir besoin de maintenance au cours de celle ci (la durée de la campagne ne fait pas dépasser l'intevalle de maintenance).
-- Lors d'une campagne, les personnes et la plateforme ne doivent pas être occupé par une autre campagne.
+- Lors d'une campagne, les personnes et la plateforme ne doivent pas être occupé par une autre campagne simultanément.
--- script.sql
+++ script.sql
@@ -12,7 +12,7 @@ CREATE TABLE HABILITATION (
 );
 
 CREATE TABLE PERSONNEL (
-    idPersonnel INT AUTO_INCREMENT,
+    idPersonnel VARCHAR(10),
     nom VARCHAR(50),
     prenom VARCHAR(50),
     mdp VARCHAR(50) UNIQUE,
@@ -39,7 +39,7 @@ CREATE TABLE EQUIPEMENT (
 CREATE TABLE PLATEFORME (
     nomPlateforme VARCHAR(50),
     nbPersonnesRequises INT,
-    coutJournalier DECIMAL(10, 2),
+    coutJournalier DECIMAL(5, 2),
     intervalleMaintenance INT,
     PRIMARY KEY (nomPlateforme)
 );
@@ -54,12 +54,12 @@ ALTER TABLE INCULRE_EQUIPEMENT ADD FOREIGN KEY (nomPlateforme) REFERENCES PLATEF
 ALTER TABLE INCULRE_EQUIPEMENT ADD FOREIGN KEY (idEquipement) REFERENCES EQUIPEMENT(idEquipement);
 
 CREATE TABLE NECESSITER_HABILITATION (
-    nomPlateforme VARCHAR(50),
+    idEquipement INT,
     idHabilitation INT,
-    PRIMARY KEY (nomPlateforme, idHabilitation)
+    PRIMARY KEY (idEquipement, idHabilitation)
 );
 
-ALTER TABLE NECESSITER_HABILITATION ADD FOREIGN KEY (nomPlateforme) REFERENCES PLATEFORME(nomPlateforme);
+ALTER TABLE NECESSITER_HABILITATION ADD FOREIGN KEY (idEquipement) REFERENCES EQUIPEMENT(idEquipement);
 ALTER TABLE NECESSITER_HABILITATION ADD FOREIGN KEY (idHabilitation) REFERENCES HABILITATION(idHabilitation);
 
 CREATE TABLE MAINTENANCE (
@@ -114,6 +114,95 @@ ALTER TABLE ECHANTILLON ADD FOREIGN KEY (idEspece) REFERENCES ESPECE(idEspece);
 
 
 
+delimiter |
 
+create or replace function remainingBudget(moment Date) returns DECIMAL(10,2)
+begin
+    declare initialBudget DECIMAL(10,2);
+
+    declare monthSearched INT default MONTH(moment);
+    declare yearSearched INT default YEAR(moment);
+
+    declare coutCampagne DECIMAL(10,2);
+    declare budgetUtilise DECIMAL(10,2);
+    declare fini default false;
+    declare lesCouts cursor for 
+        SELECT coutJournalier * duree 
+        FROM CAMPAGNE NATURAL JOIN PLATEFORME
+        WHERE MONTH(dateDebut) = monthSearched AND YEAR(dateDebut) = yearSearched;
+
+    declare continue handler for not found set fini = true; 
+
+    -- récupération du budget initiale du mois courrant a la date souhaité
+    SELECT budget into initialBudget
+    FROM BUDGET
+    WHERE YEAR(dateMoisAnnee) = yearSearched AND MONTH(dateMoisAnnee) = monthSearched;
+
+    -- somme des couts des campagne du mois courrant a la date souhaité
+
+    open lesCouts
+    while not fini do 
+        fetch lesCouts into coutCampagne;
+        if not fini then
+            set budgetUtilise = budgetUtilise + coutCampagne;
+        end if;
+    end while;
+
+    return initialBudget - coutCampagne;
+end |
+
+create or replace function plateformeAvailable(moment date,plateforme VARCHAR(50)) returns boolean
+begin
+    declare isAvailable boolean default true;
+    declare dureeCampagne INT;
+    declare dateCampagne DATE;
+
+    declare fini default false;
+    declare lesCampagnes cursor for 
+        SELECT dateDebut,duree
+        FROM CAMPAGNE
+        WHERE nomPlateforme = plateforme AND MONTH(moment) = MONTH(dateDebut) AND YEAR(moment) = YEAR(dateDebut); 
+
+    open lesCampagnes
+    while not fini do
+
+        fetch lesCampagnes into dateCampagne,dureeCampagne;
+
+
+    end while;
+
+end |
+
+-- Triggers
+
+create or replace TRIGGER checkCampagneValidity
+before INSERT ON CAMPAGNE FOR EACH ROW 
+begin
+    declare budgetRestant DECIMAL(10,2) default SELECT remainingBudget(new.dateDebut);
+    declare coutPlatefrom DECIMAL(10,2);
+
+    declare messageErreur VARCHAR(50); 
+
+    -- récupération du cout journalier de la plateforme 
+    -- utilisé pour la nouvelle campagne
+    SELECT coutJournalier into coutPlatefrom
+    FROM PLATEFORME
+    WHERE nomPlateforme = new.nomPlateforme;
+
+    -- vérification de la validité des couts de la campagne 
+    if new.duree * coutJournalier > budgetRestant then
+        set messageErreur = CONCAT('Coût dépassant du budget restant :  ',new.duree * coutJournalier,' coût de la plateforme    |   ',budgetRestant,'  budget restant.');
+        signal SQLSTATE '45000' set MESSAGE_TEXT = messageErreur;
+    end if;
+
+    -- verification de validité d'utilisation de la plateforme 
+
+
+end |
+
+delimiter ;
+
+
+ 
 
 
