69d50af9ced46fcd852b3246c8e207554c239f5e
Camera Nicolas <o22404919@or-iut-i007-f23.iut45.univ-orleans.fr>
2025-09-25 12:28:07 +0200

    Ajout d'inserts

---
+++ inserts.sql
@@ -0,0 +1,10 @@
+insert into BUDGET values('2025-09-01', 10000.00);
+
+insert into PLATEFORME values('Plateforme 1', 10, 300, 31);
+
+insert into MAINTENANCE values('Plateforme 1', '2025-09-10', false);
+
+insert into CAMPAGNE values(1, 'Plateforme 1', '2025-09-01', '30', 'Orléans');
+
+
+
