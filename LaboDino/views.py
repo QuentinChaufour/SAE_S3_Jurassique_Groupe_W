3769186641d5a6bf8cf89210ba1f0e8bbbe37720
Revan <quentin.chaufour@etu.univ-orleans.fr>
2025-11-20 18:58:42 +0100

    fix: method for global pagination, and improved docstring

--- LaboDino/views.py
+++ LaboDino/views.py
@@ -8,12 +8,17 @@ from flask_login import login_user, logout_user, login_required
 @app.route("/")
 def home():
 
-    return redirect(url_for("login"))
+    return redirect(url_for("get_campaigns"))
     #return render_template("campaign_details.html",campaign_id= 2 ,participants= {1:["a","b","c"],2:["d","e"]})
 
 @app.route("/login/", methods=["GET", "POST"])
 def login():
-    """Affiche le formulaire de connexion et gère la soumission du formulaire."""
+    """
+    Affiche le formulaire de connexion et gère la soumission du formulaire.
+    A la route : /login/
+    En cas de succès, redirige vers le menu selon le role de l'utilisateur.
+    """
+    
     form = LoginForm()
 
     if not form.is_submitted():
@@ -37,14 +42,24 @@ def login():
 
 @app.route("/logout/")
 def logout():
-    """Déconnecte l'utilisateur actuel."""
+    """
+    Déconnecte l'utilisateur actuel.
+    A la route : /logout/
+    Et le redirige vers la page de connexion.
+    """
+
     logout_user()
     return redirect(url_for("login"))
 
-@login_required
 @app.route("/budget/", methods=["GET", "POST"])
+@login_required
+@role_access_rights(UserRole.DIRECTION)
 def set_budget():
-    """Affiche le formulaire de définition du budget et gère la soumission du formulaire."""
+    """
+    Affiche le formulaire de définition du budget et gère la soumission du formulaire.
+    A la route : /budget/
+    N'est accessible qu'aux utilisateurs avec les rôles DIRECTION.
+    """
 
     form = BudgetForm()
 
@@ -59,20 +74,72 @@ def set_budget():
 
 @login_required
 @app.route("/campaigns/", methods=["GET", "POST"])
+@role_access_rights(UserRole.RESEARCHER)
 def get_campaigns():
-    """Affiche la page de présentation de l"ensembles des campagnes."""
+    """
+    Affiche la page de présentation de l"ensembles des campagnes.
+    A la route : /campaigns/
+    N'est accessible qu'aux utilisateurs avec les rôles RESEARCHER.
+    """
 
-    # Logic to retrieve and display campaigns
+    # getting all campaigns
+    data = [2,46,67,89,23,12,45,78,90,11,34,56]  # Example data
 
-    return render_template("campaign_dashboard.html", campaigns= [2,46,67])
+    # getting the page number from query parameters
+    page = request.args.get('page', 1, type=int)
 
-@app.route("/campaigns/<int:campaign_id>")
+    return render_template("campaign_dashboard.html", campaigns= _pagination(data, page), page= page)
+
+@app.route("/campaigns/create/", methods=["GET", "POST"])
 @login_required
-@role_access_rights(UserRole.TECHNICIAN,UserRole.ADMIN)
+@role_access_rights(UserRole.RESEARCHER)
+def create_campaign():
+    """
+    Affiche le formulaire de création d"une nouvelle campagne et gère la soumission du formulaire.
+    A la route : /campaigns/create
+    N'est accessible qu'aux utilisateurs avec les rôles RESEARCHER.
+    """
+
+    # TODO
+    pass
+
+@app.route("/campaigns/<int:campaign_id>/", methods=["GET", "POST"])
+@login_required
+@role_access_rights(UserRole.RESEARCHER)
 def campaign_detail(campaign_id):
-    """Affiche les détails d"une campagne spécifique."""
+    """
+    Affiche les détails d"une campagne spécifique.
+    A la route : /campaigns/<int:campaign_id> ou campaign_id est l'identifiant de la campagne.
+    N'est accessible qu'aux utilisateurs avec les rôles RESEARCHER.
+
+    Args:
+        campaign_id (int): L"identifiant de la campagne à afficher.
+    """
+
     # Logic to retrieve and display campaign details
     print(f"Campaign ID: {campaign_id}")
+
     return render_template("campaign_details.html", campaign_id=campaign_id, participants= {1:["a","b","c"],2:["d","e"]})
 
-       
+def _pagination(data: list, page: int, items_per_page: int = 5) -> list:
+    """Pagine les données en fonction de la page et du nombre d"éléments par page.
+    
+    Args:
+        data (list): La liste des données à paginer.
+        page (int): Le numéro de la page actuelle (1-indexed).
+        items_per_page (int, optional): Le nombre d"éléments par page. Par défaut à 5.
+    
+    Returns:
+        list: La sous-liste des données correspondant à la page demandée.
+    """
+
+    # checking bounds for pagination
+    if page < 1:
+        page = 1
+    elif page > (len(data) - 1) // items_per_page + 1:
+        page = (len(data) - 1) // items_per_page + 1
+
+    begin: int = (page - 1) * items_per_page
+    end: int = begin + items_per_page
+
+    return data[begin:end]
