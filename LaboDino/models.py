a4e7dcb87c0a2310b9e7cfb471c8abc8250e63f1
Bastien-MOINS <bastien.moins@gmail.com>
2025-11-20 14:01:06 +0100

    Test complet terminé avec mysql

--- LaboDino/models.py
+++ LaboDino/models.py
@@ -1,13 +1,20 @@
-from .app import db
 import enum
-from sqlalchemy import Integer, Enum
+from .app import db
 
-#from sqlalchemy.dialects.mysql import MEDIUMTEXT
 class ROLE(enum.Enum):
     administratif = 'administratif'
     chercheur = 'chercheur'
     technicien = 'technicien'
     direction = 'direction'
+
+inclure_equipement = db.Table("INCLURE_EQUIPEMENT", 
+                              db.Column("nom_plateforme_inclure", db.String(50), db.ForeignKey("PLATEFORME.nom_plateforme")), 
+                              db.Column("id_equipement_inclure", db.Integer, db.ForeignKey("EQUIPEMENT.id_equipement")))
+
+necessiter_habilitation = db.Table("NECESSITER_HABILITATION", 
+                                   db.Column("id_equipement_necessiter", db.Integer, db.ForeignKey("EQUIPEMENT.id_equipement"), primary_key=True),
+                                   db.Column("id_habilitation_necessiter", db.Integer, db.ForeignKey("HABILITATION.id_habilitation"), primary_key=True))
+
 
 class ESPECE(db.Model):
     __tablename__ = 'ESPECE'
@@ -15,67 +22,70 @@ class ESPECE(db.Model):
     nom_espece = db.Column(db.String(50))
     nom_scientifique = db.Column(db.String(100))
     genome = db.Column(db.Text)
-    echantillons = db.relationship('ECHANTILLON', back_populates='espece')
+    echantillons = db.relationship("ECHANTILLON", back_populates="espece")
 
-    def __init__(self, nom_espece, nom_scientifique=None, genome=''):
+    def __init__(self, nom_espece, nom_scientifique=nom_espece, genome=""):
         self.nom_espece = nom_espece
         self.nom_scientifique = nom_scientifique
         self.genome = genome
 
     def __repr__(self):
-        return f'<Espèce : {self.nom_espece}, {self.nom_scientifique}>'
+        return f"<Espèce : {self.nom_espece}, {self.nom_scientifique}>"
 
 class ECHANTILLON(db.Model):
     __tablename__ = 'ECHANTILLON'
     id_echantillon = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_campagne = db.Column(db.Integer,db.ForeignKey('CAMPAGNE.id_campagne'), nullable=False)
-    #fichier_sequence_adn = db.Column(MEDIUMTEXT)
-    fichier_sequence_adn = db.Column(db.Text, nullable=False)
-    id_espece = db.Column(db.Integer, db.ForeignKey('ESPECE.id_espece'), nullable=True)
+    id_campagne = db.Column(db.Integer,db.ForeignKey("CAMPAGNE.id_campagne"))
+    fichier_sequence_adn = db.Column(db.Text)
+    id_espece = db.Column(db.Integer, db.ForeignKey("ESPECE.id_espece"), nullable=True)
     commentaire = db.Column(db.Text)
-    espece = db.relationship('ESPECE', back_populates='echantillons')
-    campagne = db.relationship('CAMPAGNE', back_populates='echantillons')
+    espece = db.relationship("ESPECE", back_populates="echantillons")
+    campagne = db.relationship("CAMPAGNE", back_populates="echantillons")
 
-    def __init__(self, id_campagne, fichier_sequence_adn, id_espece=None, commentaire=''):
+    def __init__(self, id_campagne, fichier_sequence_adn, commentaire=None, id_espece=None):
         self.id_campagne = id_campagne
         self.fichier_sequence_adn = fichier_sequence_adn
         self.id_espece = id_espece
         self.commentaire = commentaire
 
     def __repr__(self):
-        return f'<Échantillon de la campagne n°{self.id_campagne} la séquence adn: {self.fichier_sequence_adn}>'
-
+        return f"<Échantillon de la campagne n°{self.id_campagne} la séquence adn: {self.fichier_sequence_adn}>"
+    
 class CAMPAGNE(db.Model):
     __tablename__ = 'CAMPAGNE'
-    id_campagne = db.Column(db.Integer, primary_key=True)
-    nom_plateforme = db.Column(db.String(50), db.ForeignKey('PLATEFORME.nom_plateforme'))
-    date_debut = db.Column(db.DATE)
+    id_campagne = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_plateforme = db.Column(db.String(50), db.ForeignKey("PLATEFORME.nom_plateforme"))
+    dateDebut = db.Column(db.DATE)
     duree = db.Column(db.Integer)
     lieu = db.Column(db.String(100))
     valide = db.Column(db.Boolean)
-    personnel = db.relationship('PERSONNEL', secondary='PARTICIPER_CAMPAGNE', back_populates='campagne')
-    plateforme = db.relationship('PLATEFORME', back_populates='campagnes')
-    echantillons = db.relationship('ECHANTILLON', back_populates='campagne')
+    participerCampagne = db.relationship("PARTICIPER_CAMPAGNE", back_populates="campagne")
+    plateforme = db.relationship("PLATEFORME", back_populates="campagnes")
+    echantillons = db.relationship("ECHANTILLON", back_populates="campagne")
 
-    def __init__(self, nom_plateforme, date_debut, duree, lieu, valide=False ):
+
+    def __init__(self, nom_plateforme, dateDebut, duree, lieu, valide=False ):
         self.nom_plateforme = nom_plateforme
-        self.date_debut = date_debut
+        self.dateDebut = dateDebut
         self.duree = duree
         self.lieu = lieu
         self.valide = valide
 
     def __repr__(self):
-        return f'<Campagne n°{self.id_campagne} sur la plateforme {self.nom_plateforme} a commencé le {self.date_debut} pour une durée de {self.duree} heure(s)>'
+        return f"<Campagne n°{self.id_campagne} sur la plateforme {self.nom_plateforme} a commencé le {self.dateDebut} pour une durée de {self.duree}>"
+
+
 
 class PERSONNEL(db.Model):
-    __tablename__ = 'PERSONNEL'
+    __tablename__ = "PERSONNEL"
     id_personnel = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nom = db.Column(db.String(50))
     prenom = db.Column(db.String(50))
     mdp = db.Column(db.String(10), unique=True)
     role = db.Column(db.Enum(ROLE))
-    campagne = db.relationship('CAMPAGNE', secondary='PARTICIPER_CAMPAGNE', back_populates='personnel')
-    habilitation = db.relationship('HABILITATION', secondary='POSSEDER', back_populates='personnel')
+
+    participerCampagne = db.relationship("PARTICIPER_CAMPAGNE", back_populates="personnel")
+    posseder = db.relationship("POSSEDER", back_populates="personnels")
 
     def __init__(self, nom, prenom, mdp, role):
         self.nom = nom
@@ -84,21 +94,40 @@ class PERSONNEL(db.Model):
         self.role = role
 
     def __repr__(self):
-        return f'<{self.nom} {self.prenom} : {self.role}>'
+        return f"<{self.nom} {self.prenom} : {self.role}>"
 
-class HABILITATION(db.Model):
-    __tablename__ = 'HABILITATION'
-    
-    id_habilitation = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    nom_habilitation = db.Column(db.String(50))
-    equipements = db.relationship('EQUIPEMENT', secondary='necessiter_habilitation', back_populates='habilitations')
-    personnel = db.relationship('PERSONNEL',secondary='POSSEDER', back_populates='habilitation')
+class PARTICIPER_CAMPAGNE(db.Model):
+    __tablename__ = "PARTICIPER_CAMPAGNE"
+    id_campagne = db.Column( db.Integer, db.ForeignKey("CAMPAGNE.id_campagne"),primary_key=True)
+    id_personnel = db.Column(db.Integer, db.ForeignKey("PERSONNEL.id_personnel"),primary_key=True)
+    campagne = db.relationship("CAMPAGNE", back_populates="participerCampagne")
+    personnel = db.relationship("PERSONNEL", back_populates="participerCampagne")
 
-    def __init__(self, nom_habilitation=''):
-        self.nom_habilitation = nom_habilitation
+    def __init__(self, id_campagne, id_personnel):
+        self.id_campagne = id_campagne
+        self.id_personnel = id_personnel
 
     def __repr__(self):
-        return 'Habilitation : ' + self.nom_habilitation
+        nom = getattr(self.personnel, 'nom', None)
+        prenom = getattr(self.personnel, 'prenom', None)
+        return f"<POSSEDER campagne= n°{self.id_campagne} personnel={nom!r} {prenom!r}>"
+
+class POSSEDER(db.Model):
+    __tablename__ = "POSSEDER_HABILITATION"
+    id_personnel = db.Column(db.Integer, db.ForeignKey("PERSONNEL.id_personnel"),primary_key=True)
+    id_habilitation = db.Column(db.Integer, db.ForeignKey("HABILITATION.id_habilitation"), primary_key=True)
+    personnels = db.relationship("PERSONNEL", back_populates="posseder")
+    habilitations = db.relationship("HABILITATION", back_populates="posseder")
+
+    def __init__(self, id_habilitation, id_personnel):
+        self.id_habilitation = id_habilitation
+        self.id_personnel = id_personnel
+
+    def __repr__(self):
+        hab = getattr(self.habilitation, 'nom_habilitation', None)
+        nom = getattr(self.personnel, 'nom', None)
+        prenom = getattr(self.personnel, 'prenom', None)
+        return f"<POSSEDER habilitation={hab!r} personnel={nom!r} {prenom!r}>"
 
 class BUDGET(db.Model):
     __tablename__ = 'BUDGET'
@@ -113,6 +142,7 @@ class BUDGET(db.Model):
     def __repr__(self):
         return 'Budget : ' + str(self.date_mois_annee)
 
+
 class PLATEFORME(db.Model):
     __tablename__ = 'PLATEFORME'
     
@@ -121,11 +151,12 @@ class PLATEFORME(db.Model):
     cout_journalier = db.Column(db.Numeric(10, 2))
     intervalle_maintenance = db.Column(db.Integer)
     
-    equipements = db.relationship('EQUIPEMENT', secondary='inclure_equipement', back_populates='plateformes')
+    equipements = db.relationship('EQUIPEMENT', secondary=inclure_equipement, back_populates='plateformes')
     maintenances = db.relationship('MAINTENANCE', back_populates='plateforme')
     campagnes = db.relationship('CAMPAGNE', back_populates='plateforme')
+    
 
-    def __init__(self, nom_plateforme='', nb_personnes_requises=0, cout_journalier=0, intervalle_maintenance=0):
+    def __init__(self, nom_plateforme="", nb_personnes_requises=0, cout_journalier=0, intervalle_maintenance=0):
         self.nom_plateforme = nom_plateforme
         self.nb_personnes_requises = nb_personnes_requises
         self.cout_journalier = cout_journalier
@@ -133,6 +164,22 @@ class PLATEFORME(db.Model):
 
     def __repr__(self):
         return 'Plateforme : ' + self.nom_plateforme
+    
+
+class HABILITATION(db.Model):
+    __tablename__ = 'HABILITATION'
+    
+    id_habilitation = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    nom_habilitation = db.Column(db.String(50))
+
+    equipements = db.relationship('EQUIPEMENT', secondary="NECESSITER_HABILITATION", back_populates='habilitations')
+    posseder = db.relationship('POSSEDER', back_populates='habilitations')
+
+    def __init__(self, nom_habilitation=""):
+        self.nom_habilitation = nom_habilitation
+
+    def __repr__(self):
+        return 'Habilitation : ' + self.nom_habilitation
 
 class EQUIPEMENT(db.Model):
     __tablename__ = 'EQUIPEMENT'
@@ -140,9 +187,10 @@ class EQUIPEMENT(db.Model):
     id_equipement = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nom_equipement = db.Column(db.String(50))
     
-    plateformes = db.relationship('PLATEFORME', secondary='inclure_equipement', back_populates='equipements')
-    habilitations = db.relationship('HABILITATION', secondary='necessiter_habilitation', back_populates='equipements')
-    def __init__(self, nom_equipement=''):
+    plateformes = db.relationship('PLATEFORME', secondary=inclure_equipement, back_populates='equipements')
+    habilitations = db.relationship('HABILITATION', secondary="NECESSITER_HABILITATION", back_populates='equipements')
+
+    def __init__(self, nom_equipement=""):
         self.nom_equipement = nom_equipement
 
     def __repr__(self):
@@ -157,26 +205,10 @@ class MAINTENANCE(db.Model):
     
     plateforme = db.relationship('PLATEFORME', back_populates='maintenances')
 
-    def __init__(self, nom_plateforme='', date_maintenance=None, duree_maintenance=0):
+    def __init__(self, nom_plateforme="", date_maintenance=None, duree_maintenance=0):
         self.nom_plateforme = nom_plateforme
         self.date_maintenance = date_maintenance
         self.duree_maintenance = duree_maintenance
 
     def __repr__(self):
         return 'Maintenance : ' + self.nom_plateforme
-
-posseder = db.Table('POSSEDER',
-                    db.Column('id_personnel', db.Integer, db.ForeignKey('PERSONNEL.id_personnel'),primary_key=True),
-                    db.Column('id_habilitation', db.Integer, db.ForeignKey('HABILITATION.id_habilitation'), primary_key=True))
-
-participer_campagne = db.Table('PARTICIPER_CAMPAGNE',
-                                    db.Column('id_campagne', db.Integer, db.ForeignKey('CAMPAGNE.id_campagne'),primary_key=True),
-                                    db.Column('id_personnel', db.Integer, db.ForeignKey('PERSONNEL.id_personnel'),primary_key=True))
-
-necessiter_habilitation = db.Table('necessiter_habilitation', 
-                                   db.Column('id_equipement_necessiter', db.Integer, db.ForeignKey('EQUIPEMENT.id_equipement')),
-                                   db.Column('id_habilitation_necessiter', db.Integer, db.ForeignKey('HABILITATION.id_habilitation')))
-
-inclure_equipement = db.Table('inclure_equipement', 
-                              db.Column('nom_plateforme_inclure', db.String(50), db.ForeignKey('PLATEFORME.nom_plateforme')), 
-                              db.Column('id_equipement_inclure', db.Integer, db.ForeignKey('EQUIPEMENT.id_equipement')))
--- tests/models_test.py
+++ tests/models_test.py
@@ -1,113 +1,181 @@
-from LaboDino.models import ROLE, PLATEFORME, PERSONNEL, CAMPAGNE, ECHANTILLON, ESPECE
-# Importer l'objet app en plus de db
+from LaboDino.models import ROLE, PLATEFORME, PERSONNEL, CAMPAGNE, ECHANTILLON, ESPECE, HABILITATION, POSSEDER, PARTICIPER_CAMPAGNE
 from LaboDino.app import db, app
 from sqlalchemy import text
-import datetime
+from datetime import date
 
-def models_test():
-    plateforme = PLATEFORME("Plateforme 1", 5, 50, 10)
-    db.session.add(plateforme)
-    db.session.commit()
-    
-    # Ajout d'un print pour voir le résultat
-    print("Plateformes:", PLATEFORME.query.all())
-
-def personnel_tests():
-    alice = PERSONNEL("Thymefield","Alice", "symmetrie1", ROLE.chercheur)
-    db.session.add(alice)
-    db.session.commit()
-
-    print("Personnels:", PERSONNEL.query.all())
-
-def campagne_tests():
-    campagne = CAMPAGNE("Plateforme 1", datetime.date(2024,1,1), 30, "Jurassic Park", True)
-    db.session.add(campagne)
-    db.session.commit()
-
-    print("Campagnes:", CAMPAGNE.query.all())
-
-def participer_campagne_tests():
-    alice = PERSONNEL("Wheel","Piper", "track2ter", ROLE.technicien)
-    campagne2 = CAMPAGNE("Plateforme 1", datetime.date(2024,1,1), 30, "Jurassico Park", True)
-    db.session.add(alice)
-    db.session.add(campagne2)
-    alice.campagne.append(campagne2)
-    db.session.commit()
-
-
-    print(f"Campagnes d'Alice : {alice.campagne}")
-    print(f"Personnel de la campagne : {campagne2.personnel}")
-
-def utiliser_tests():
-    campagne3 = CAMPAGNE("Plateforme 2", datetime.date(2024,1,1), 30, "Jurassica Park", True)
-    plateforme2 = PLATEFORME("Plateforme 2", 2, 30, 10)
-    db.session.add_all([campagne3, plateforme2])
-    plateforme2.campagnes.append(campagne3)
-    db.session.commit()
-
-    print(f"La plateforme de la campagne 3 : {campagne3.plateforme}")
-    print(f"Campagnes de la plateforme 2: {plateforme2.campagnes}")
-
-def espece_tests():
-    calamar = ESPECE("Calamar","Loligo vulgaris","ACGTTA")
-    db.session.add(calamar)
-    db.session.commit()
-
-def echantillon_tests():
-    #Erreur: echantillon1 = ECHANTILLON(7, "adn.adn", "cette table est incorrect")
-    echantillon = ECHANTILLON(3, "mystere.adn", "echantillon d'une espèce inconnu")
-    db.session.add(echantillon)
-    db.session.commit()
-    print("ECHANTILLONS:", ECHANTILLON.query.all())
-
-def appartenir_tests():
-    echantillon1 = ECHANTILLON(1, "loligo.adn", "une espèce marine avec des tentacules...")
-    echantillon2 = ECHANTILLON(1, "vulgaris.adn", "une espèce marine avec une tête ovale...")
-    poulpe = ESPECE("Poulpe","Octopus vulgaris","AGGTTC")
-    poulpe_espion = ESPECE("Poulpe?")
-    db.session.add_all([echantillon1, echantillon2, poulpe, poulpe_espion])
-    echantillon1.espece.append(poulpe)
-    echantillon2.espece.append(poulpe)
-    #Erreur: echantillon.espece.append(poulpe_espion)
-    db.session.commit()
-    print(f"L'echantillon1 vient de l'animal : {echantillon1.espece}")
-    print(f"L'espèce poulpe possède comme echantillons : {poulpe.echantillons}")
-
-def recolter_tests():
-    echantillon = ECHANTILLON(2, "test.adn", "UNE ESPECE TEST")
-    campagne4 = CAMPAGNE("Plateforme 1", datetime.date(2025, 10, 21), 20, "Test Park", True)
-    db.session.add_all([echantillon, campagne4])
-    campagne4.echantillons.append(echantillon)
-    db.session.commit()
-    print(f"L'echantillon vient de la campagne : {echantillon.campagne} donc {echantillon.id_campagne}")
-
-# Mettre en place le contexte d'application avant d'appeler les fonctions
-with app.app_context():
-    print("--- Nettoyage de la base de données ---")
-    
-    # 1. ON DÉSACTIVE LA SÉCURITÉ
+def clear_database():
+    """Nettoie la base de données en supprimant et recréant toutes les tables."""
+    # Désactiver la vérification des clés étrangères
     db.session.execute(text("SET FOREIGN_KEY_CHECKS = 0;"))
     db.session.commit()
     
+    # Supprimer toutes les tables
     db.drop_all()
     
-    # 3. ON RÉACTIVE LA SÉCURITÉ
-    db.session.execute(text("SET FOREIGN_KEY_CHECKS = 1;"))
-    db.session.commit()
-    
+    # Recréer toutes les tables
     db.create_all()
     
-    print("--- Démarrage des tests ---")
+    # Réactiver la vérification des clés étrangères
+    db.session.execute(text("SET FOREIGN_KEY_CHECKS = 1;"))
+    db.session.commit()
+
+def test_personnel():
+    """Teste la création d'entités PERSONNEL."""
+    personnel1 = PERSONNEL("Dupont", "Jean", "mdp123", ROLE.administratif)
+    personnel2 = PERSONNEL("Martin", "Sophie", "mdp456", ROLE.chercheur)
+    personnel3 = PERSONNEL("Bernard", "Luc", "mdp789", ROLE.technicien)
+    personnel4 = PERSONNEL("Dubois", "Mairie", "mdp101", ROLE.direction)
+    db.session.add_all([personnel1, personnel2, personnel3, personnel4])
+    db.session.commit()
     
-    models_test()
-    personnel_tests()
-    campagne_tests()
-    participer_campagne_tests()
-    utiliser_tests()
-    espece_tests()
-    echantillon_tests()
-    appartenir_tests()
-    recolter_tests()
+    assert PERSONNEL.query.count() == 4
+    assert PERSONNEL.query.filter_by(nom="Dupont").first().prenom == "Jean"
+    assert PERSONNEL.query.filter_by(nom="Martin").first().role == ROLE.chercheur
+    assert PERSONNEL.query.filter_by(role=ROLE.direction).count() == 1
+
+def test_habilitations():
+    """Teste la création d'entités HABILITATION."""
+    hab1 = HABILITATION("Manipulation ADN")
+    hab2 = HABILITATION("Séquençage")
+    hab3 = HABILITATION(nom_habilitation="Analyse génomique")
+    db.session.add_all([hab1, hab2, hab3])
+    db.session.commit()
     
-    print("--- Tests terminés ---") 
+    assert HABILITATION.query.count() == 3
+    assert HABILITATION.query.filter_by(nom_habilitation="Séquençage").first() is not None
+    assert hab1.id_habilitation is not None
+
+def test_plateformes():
+    """Teste la création d'entités PLATEFORME."""
+    plat1 = PLATEFORME("Plateforme A", 5, 1500, 90)
+    plat2 = PLATEFORME("Plateforme B", 3, 800, 60)
+    db.session.add_all([plat1, plat2])
+    db.session.commit()
+
+    assert PLATEFORME.query.count() == 2
+    assert PLATEFORME.query.filter_by(nom_plateforme="Plateforme A").first().nb_personnes_requises == 5
+
+def test_campagnes_et_utiliser():
+    """Teste la création de CAMPAGNE et l'association UTILISER avec PLATEFORME."""
+    plateforme = PLATEFORME.query.filter_by(nom_plateforme="Plateforme A").first()
+    
+    camp1 = CAMPAGNE(plateforme.nom_plateforme, date(2024, 1, 15), 30, "Montana, USA", True)
+    camp2 = CAMPAGNE(plateforme.nom_plateforme, date(2024, 3, 10), 45, "Patagonie, Argentine", True)
+    camp3 = CAMPAGNE(plateforme.nom_plateforme, date(2024, 7, 12), 50, "Le Caire, Égypte", True)
+    
+    db.session.add_all([camp1, camp2, camp3])
+    db.session.commit()
+    
+    assert CAMPAGNE.query.count() == 3
+    assert CAMPAGNE.query.filter_by(lieu="Montana, USA").first().valide is True
+    assert camp1.plateforme.nom_plateforme == "Plateforme A"
+    assert len(plateforme.campagnes) == 3
+
+def test_participer_campagne():
+    """Teste l'association PARTICIPER_CAMPAGNE entre CAMPAGNE et PERSONNEL."""
+    personnel = PERSONNEL.query.all()
+    campagnes = CAMPAGNE.query.all()
+    
+    part_camp1 = PARTICIPER_CAMPAGNE(campagnes[0].id_campagne, personnel[0].id_personnel)
+    part_camp2 = PARTICIPER_CAMPAGNE(campagnes[1].id_campagne, personnel[1].id_personnel)
+    part_camp3 = PARTICIPER_CAMPAGNE(campagnes[0].id_campagne, personnel[2].id_personnel)
+    part_camp4 = PARTICIPER_CAMPAGNE(campagnes[2].id_campagne, personnel[2].id_personnel)
+    
+    db.session.add_all([part_camp1, part_camp2, part_camp3, part_camp4])
+    db.session.commit()
+    
+    assert PARTICIPER_CAMPAGNE.query.count() == 4
+    assert PARTICIPER_CAMPAGNE.query.filter_by(id_personnel=personnel[2].id_personnel).count() == 2
+    assert len(personnel[2].posseder) == 2
+
+def test_posseder():
+    """Teste l'association POSSEDER entre HABILITATION et PERSONNEL."""
+    personnel = PERSONNEL.query.all()
+    habilitations = HABILITATION.query.all()
+    
+    poss1 = POSSEDER(habilitations[0].id_habilitation, personnel[0].id_personnel)
+    poss2 = POSSEDER(habilitations[1].id_habilitation, personnel[1].id_personnel)
+    poss3 = POSSEDER(habilitations[0].id_habilitation, personnel[2].id_personnel)
+    poss4 = POSSEDER(habilitations[2].id_habilitation, personnel[2].id_personnel)
+    
+    db.session.add_all([poss1, poss2, poss3, poss4])
+    db.session.commit()
+    
+    assert POSSEDER.query.count() == 4
+    assert POSSEDER.query.filter_by(id_personnel=personnel[2].id_personnel).count() == 2
+    assert len(personnel[2].posseder) == 2
+
+def test_especes():
+    """Teste la création d'entités ESPECE."""
+    espece1 = ESPECE("T-Rex", "Tyrannosaurus Rex", "AGCT...")
+    espece2 = ESPECE("Triceratops", "Triceratops horridus", "GTCA...")
+    db.session.add_all([espece1, espece2])
+    db.session.commit()
+
+    assert ESPECE.query.count() == 2
+    assert ESPECE.query.filter_by(nom_espece="T-Rex").first().genome == "AGCT..."
+
+def test_echantillons():
+    """Teste la création d'entités ECHANTILLON."""
+    echantillon1 = ECHANTILLON(1, "TRex_dent.adn", "Dent de T-Rex")
+    echantillon2 = ECHANTILLON(2, "Tric_corne.adn", "Corne de Triceratops")
+    db.session.add_all([echantillon1, echantillon2])
+    db.session.commit()
+
+    assert ECHANTILLON.query.count() == 2
+    assert ECHANTILLON.query.filter_by(commentaire="Dent de T-Rex").first() is not None
+
+def test_appartenir():
+    """Teste l'association APPARTENIR entre ECHANTILLON et ESPECE."""
+    echantillon = ECHANTILLON.query.filter_by(commentaire="Dent de T-Rex").first()
+    espece = ESPECE.query.filter_by(nom_espece="T-Rex").first()
+    
+    echantillon.espece = espece
+    db.session.commit()
+    
+    assert echantillon.espece.nom_espece == "T-Rex"
+    assert len(espece.echantillons) == 1
+    assert espece.echantillons[0].commentaire == "Dent de T-Rex"
+
+def test_recolter():
+    """Teste l'association RECOLTER entre ECHANTILLON et CAMPAGNE."""
+    echantillon = ECHANTILLON.query.filter_by(commentaire="Corne de Triceratops").first()
+    campagne = CAMPAGNE.query.filter_by(lieu="Patagonie, Argentine").first()
+    
+    echantillon.campagne = campagne
+    db.session.commit()
+    
+    assert echantillon.campagne.lieu == "Patagonie, Argentine"
+    assert len(campagne.echantillons) == 1
+    assert campagne.echantillons[0].commentaire == "Corne de Triceratops"
+
+def run_tests():
+    """Exécute tous les tests dans le contexte de l'application."""
+    with app.app_context():
+        print("--- Nettoyage de la base de données ---")
+        clear_database()
+        
+        print("--- Démarrage des tests ---")
+        
+
+        test_personnel()
+        test_habilitations()
+
+        test_posseder()
+        
+        test_plateformes()
+        test_especes()
+
+        test_campagnes_et_utiliser()
+
+        test_echantillons()
+        
+        test_participer_campagne()
+        test_appartenir()
+        test_recolter()
+        
+        print("--- Tests terminés avec succès ---")
+
+if __name__ == "__main__":
+    run_tests()
 
